fc14dae0f76bdc5a55afa76c0af3e521c5017155
Himanshu Agrawal <himanshuagrawal1998@gmail.com>
2019-07-24 16:03:47 +0530

    add restore functions

--- main.go
+++ main.go
@@ -24,12 +24,15 @@ func main() {
 	awsProfile := flag.String("awsProfile", "default", "a string")
 	region := flag.String("region", "us-east-1", "a string")
 	bucket := flag.String("bucket", "laptop-db", "a string")
+	command := flag.String("command", "backup", "backup/restore")
+	days := flag.Int("days", 0, "days from now to restore")
 	// host := flag.String("host", "127.0.0.1", "a string")
 	// username := flag.String("username", "cassandra", "a string")
 	// password := flag.String("password", "cassandra", "a string")
 	keyspace := flag.String("keyspace", "", "a string")
 	incremental := flag.Bool("incremental", false, "a bool")
-	cassandraDataDir := flag.String("cassandraDataDir", "/var/lib/cassandra/data", "a string")
+	cassandraDataDir := flag.String("cassandraDataDir", "/var/lib/cassandra/data", "a path")
+	restoreDataDir := flag.String("restoreDataDir", "/tmp/data", "a path")
 
 	flag.Parse()
 
@@ -41,27 +44,122 @@ func main() {
 		fmt.Print(err.Error())
 	}
 	svcS3 := s3.New(sess)
+
 	uploader := s3manager.NewUploaderWithClient(svcS3, func(u *s3manager.Uploader) {
 		u.MaxUploadParts = 10000       // set to maximum allowed by s3
 		u.PartSize = 128 * 1024 * 1024 // 128MB
 	})
+	downloader := s3manager.NewDownloaderWithClient(svcS3, func(d *s3manager.Downloader) {
+		d.PartSize = 64 * 1024 * 1024 // 64MB per part
+	})
 
-	fmt.Print("AWS Session created")
+	fmt.Println("AWS Session created")
 
-	if *incremental == true {
-		resp, err := IncrementalSnapshot(svcS3, *cassandraDataDir, *bucket, uploader, *keyspace)
-		if err != nil {
-			fmt.Print(err.Error())
+	if *command == "backup" {
+		if *incremental == true {
+			resp, err := IncrementalSnapshot(svcS3, *cassandraDataDir, *bucket, uploader, *keyspace)
+			if err != nil {
+				fmt.Print(err.Error())
+			}
+			fmt.Print(resp)
+		} else {
+			resp, err := FullSnapshot(svcS3, *cassandraDataDir, *bucket, uploader, *keyspace)
+			if err != nil {
+				fmt.Print(err.Error())
+			}
+			fmt.Print(resp)
 		}
-		fmt.Print(resp)
 	} else {
-		resp, err := FullSnapshot(svcS3, *cassandraDataDir, *bucket, uploader, *keyspace)
+		resp, err := RestoreByDays(svcS3, *restoreDataDir, *bucket, downloader, *keyspace, *days)
 		if err != nil {
 			fmt.Print(err.Error())
 		}
 		fmt.Print(resp)
+		fmt.Print("Downloaded Snapshot. To restore move the files to /tmp/data/keyspace_name/table_name/mc-xxxx-data.DB and run sstableloader -d 0.0.0.0 /tmp/data/keyspace_name/table_name")
 	}
 
+}
+
+func RestoreByDays(svcS3 *s3.S3, cassandraDataDir string, bucket string, downloader *s3manager.Downloader, keyspace string, days int) (string, error) {
+	now := time.Now()
+	t := now.AddDate(0, 0, -days)
+	prefix := t.Format("2006/01/02/")
+	fmt.Println("Downloading for " + prefix)
+	listObjectsInput := s3.ListObjectsInput{
+		Bucket: &bucket,
+		Prefix: &prefix,
+	}
+	keys, err := svcS3.ListObjects(&listObjectsInput)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	// objects := []s3manager.BatchDownloadObject{}
+	for _, key := range (*keys).Contents {
+		localKey := (*key.Key)[11:]
+		if keyspace == "" || strings.Index(localKey, keyspace) == 0 {
+			fmt.Println("Downloading " + *key.Key)
+			localDir := cassandraDataDir + "/" + localKey
+			localDirPos := strings.LastIndex(localDir, "/")
+			localDir = localDir[:localDirPos]
+			fmt.Println(localDir)
+			err := os.MkdirAll(localDir, 0777)
+			if err != nil {
+				log.Fatal("error creating "+localDir, err.Error())
+			}
+
+			fileName := cassandraDataDir + "/" + localKey
+
+			params := &s3.GetObjectInput{
+				Bucket: aws.String(bucket),
+				Key:    key.Key,
+			}
+			resp, err := svcS3.GetObject(params)
+			if err != nil {
+				return "", err
+			}
+
+			file, err := os.Create(fileName)
+			if err != nil {
+				return "", err
+			}
+
+			reader, writer := io.Pipe()
+			go func() {
+				gr, err := gzip.NewReader(resp.Body)
+				if err != nil {
+					log.Fatal("error creating new gzip reader")
+					os.Exit(1)
+				}
+				io.Copy(writer, gr)
+				gr.Close()
+				writer.Close()
+			}()
+
+			buf := make([]byte, 1024)
+			for {
+				n, err := reader.Read(buf)
+				if err != nil && err != io.EOF {
+					return "", err
+				}
+				if n == 0 {
+					break
+				}
+				if _, writeErr := file.Write(buf[:n]); writeErr != nil {
+					return "", err
+				}
+			}
+
+			if err = reader.Close(); err != nil {
+				return "", err
+			}
+			if err = file.Close(); err != nil {
+				return "", err
+			}
+		}
+	}
+
+	return "", nil
 }
 
 func IncrementalSnapshot(svcS3 *s3.S3, cassandraDataDir string, bucket string, uploader *s3manager.Uploader, keyspace string) (string, error) {
